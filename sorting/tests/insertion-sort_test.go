1a1629a3bea83f53d7e67e493fa854946e21fb20
Sergey Suvorov <sergey.personal@gmail.com>
2022-04-02 21:54:24 +0300

    update tests

--- sorting/tests/insertion-sort_test.go
+++ sorting/tests/insertion-sort_test.go
@@ -14,24 +14,34 @@ func TestInsertionSorting(t *testing.T) {
 		want  []int
 	}{
 		{
-			name:  "test1",
-			input: []int{6, 8, 2, 9, 10, 1, 3, 7, 5, 0, 4},
+			name:  "sorted slice",
+			input: []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			want:  []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 		},
 		{
-			name:  "test2",
+			name:  "reversed slice",
 			input: []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
 			want:  []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 		},
 		{
-			name:  "test3",
+			name:  "random unsorted",
+			input: []int{6, 8, 2, 9, 10, 1, 3, 7, 5, 0, 4},
+			want:  []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		},
+		{
+			name:  "one value",
 			input: []int{5},
 			want:  []int{5},
 		},
 		{
-			name:  "test4",
+			name:  "same values",
 			input: []int{5, 5, 5, 5, 5, 5, 5, 5},
 			want:  []int{5, 5, 5, 5, 5, 5, 5, 5},
+		},
+		{
+			name:  "empty slice",
+			input: []int{},
+			want:  []int{},
 		},
 	}
 
